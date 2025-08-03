0a9cbac4e6794081d1ac1e6935c72f5894eaba29
Bharath-Kumaar-sk <BharathKumaarskbk05@gmail.com>
2025-08-03 20:42:07 +0530

    Try_toGetLines

--- textBlock.py
+++ textBlock.py
@@ -9,7 +9,11 @@ section_keyword = ["EDUCATION", "EXPERIENCE", "SKILLS", "PROJECTS", "COURSEWORK"
 with fitz.open(fname) as doc:
 
     sections = {}
+    font_dict= {}
+    size_dict = {}
+    current_font = None
     current_section = None
+    current_size = None
 
     for page in doc:
         page_width = page.rect.width
@@ -19,44 +23,59 @@ with fitz.open(fname) as doc:
         sorted_blocks = sorted(text_dict["blocks"], key=lambda b:(b["bbox"][1], b["bbox"][0]))
 
         for block in sorted_blocks:
-            if block["type"] == 0:
+            if block["type"] == 0: #0 means Text
                 for line in block["lines"]:
+                    line_text = ""
+                    max_size = 0
+                    span_font = ""
+
                     for span in line["spans"]:
-                        text = span["text"]
                         #getting details
-                        if not text:
-                            continue
-                        span_x0 = span["bbox"][0]
-                        font = span["font"]
+                        line_text += span["text"] #building line text
                         size = span["size"]
-                        #checking for left and right
-                        if span_x0 < midpoint:
-                            column = "left"
-                        elif span_x0 >= midpoint:
-                            column = "right" 
+                        if max_size < size: #max size in each line
+                             max_size = size
+                    
+                    #Now a full line is created
+                    span_x0 = span["bbox"][0] #to check left column and right column
+                    for Chksize in line_text:
+                          if max_size == line_text["size"]:
+                                span_font = line_text["font"] #to get the font of largest size
+                    #checking for left and right
+                    if span_x0 < midpoint:
+                        column = "left"
+                    elif span_x0 >= midpoint:
+                        column = "right" 
                         #Now checking for keyword
-                        upper_text = text.upper()
-                        is_heading = False
-                        for keyword in section_keyword:
-                            if keyword in upper_text:   #checking via section_keywords using flag
+                    
+                    upper_text = line_text.upper()
+                    is_heading = False
+                    for keyword in section_keyword:
+                        if keyword in upper_text:   #checking via section_keywords using flag
                                 current_section = keyword
+                                current_font = span_font
+                                current_size = max_size
                                 is_heading = True
                                 break
                                 
                             #checking via font size and style
-                        if current_section == None and not is_heading:
-                            if ("Bold" in font and size > 12):
+                    if current_section == None and not is_heading:
+                        if ("Bold" in span_font and size > 12):
                                 current_section = "Heading"
-                            elif ("Bold" in font and size < 12):
+                        elif ("Bold" in span_font and size < 12):
                                 current_section = "section heading"
-                            elif (not "Bold" in font and size < 12):
+                        elif (not "Bold" in span_font and size < 12):
                                 current_section = "Body"
                         
-                        if current_section not in sections:
+                    if current_section not in sections:
                                 sections[current_section] = []
+                                font_dict[current_font] = []
+                                size_dict[current_size] = []
 
-                        if not is_heading:
-                            sections[current_section].append(text) #to append text regardless of section
+                    if not is_heading:
+                            sections[current_section].append(line_text) #to append text regardless of section
+                            font_dict[current_font].append(span_font)
+                            size_dict[current_size].append(max_size)
 
 print("Output\n")
 for section, text in sections.items():
