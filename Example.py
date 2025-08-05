023a7f3d0637721c1f568b8a8c0450aa85fd7ed0
Bharath-Kumaar-sk <BharathKumaarskbk05@gmail.com>
2025-08-05 14:26:25 +0530

    Paragraph

--- Example.py
+++ Example.py
@@ -23,7 +23,9 @@ with fitz.open(fname) as doc:
         for block in sorted_blocks:
             if block["type"] == 0:
                 prev_y1 = None
+                paragraph_lines = []
                 for line in block["lines"]:
+                    
                     line_text = ""
                     max_size = 0
                     span_font = ""
@@ -34,7 +36,7 @@ with fitz.open(fname) as doc:
                         size = span["size"]
                         if max_size < size:
                             max_size = size
-                            font = span["font"]
+                            span_font = span["font"]
                         
                     line_y0 = line["bbox"][1] #calculating y0 y1 outside of span
                     line_y1 = line["bbox"][3] 
@@ -54,18 +56,35 @@ with fitz.open(fname) as doc:
                               current_section = "Section heading"
                          elif (not "Bold" in span_font and max_size < 15):
                               current_section = "Body"
-                            
-                    if current_section not in sections:
-                         sections[current_section] = []
-                         if prev_y1 == None:
-                            paragraph_lines = []
-                         elif line_y0 - prev_y1 < threshold:
-                            paragraph_lines.append(line_text)
-                         else:
-                            paragraph_lines = []
-                         sections[current_section].append(line_text)
-        
+
+                    if span_font not in para_font:
+                        para_font[span_font] = []  
+                    para_font[span_font].append(span_font)
+                    unique_font.add(span_font)    
+
+                    if max_size not in para_size:
+                        para_size[max_size] = []
+                    para_size[max_size].append(max_size)
+                    unique_size.add(max_size)
+
+                    if prev_y1 == None:
+                        paragraph_lines = [line_text]
+                    elif line_y0 - prev_y1 < threshold:
+                        paragraph_lines.append(line_text)
+                    else:
+                        if current_section not in sections:
+                            sections[current_section] = []
+                        sections[current_section].append(" ".join(paragraph_lines))
+                        paragraph_lines = []
+                    
                     prev_y1 = line_y1
+
+                if paragraph_lines:
+                    if current_section not in sections:
+                       sections[current_section] = []
+                    sections[current_section].append(" ".join(paragraph_lines))
+                    paragraph_lines = []
+
 
 
 
